337af4e3d19b51b406a9f45a09823f77ef0aaec4
tenderoalbertalberto-rgb <tenderoalbertalberto@gmail.com>
2026-08-28 15:11:32 +0200

    chore: add next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
